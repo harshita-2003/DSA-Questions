335b7ecd5ac7435415697399124bc4e94df9ddea
harshita-2003 <harshitag374@gmail.com>
2023-06-17 00:43:17 +0530

    added bubble sort

--- index.java
+++ index.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class index{
 
     //FIND ELEMENT IN MOUNTAIN ARRAY
@@ -127,5 +129,28 @@ public class index{
 
         return end;
     }
+
+
+
+    //BUBBLE SORT
+    public static void main(String[] args) {
+        int[] array = {3 , 1 , 5 , 4 , 2};
+        bubble(array);
+        System.out.println(Arrays.toString(array));
+    }
     
+    static void bubble(int[] arr) {
+        //n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            //dec by 1 since it gets sorted from end
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+    }
 }
